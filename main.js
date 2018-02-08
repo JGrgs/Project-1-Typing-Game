5661e025119b2b77655c538522b30ff5380cdc97
Jad Georges <jad.grgs@gmail.com>
2018-02-08 10:36:03 -0800

    added functionality to change words when matched

--- main.js
+++ main.js
@@ -37,3 +37,47 @@ $word5.text(newDW5);
 var newWord6 = randomInt(displayedWords.length);
 var newDW6 = displayedWords[newWord6];
 $word6.text(newDW6);
+
+//functionality to change words when typing matches
+function changeWordOnMatch(e) {
+    if(e.keyCode === 13){
+        var inputField = $("#input-field").val()
+   
+        if (inputField === newDW1) {
+            newWord = randomInt(displayedWords.length);
+            newDW1 = displayedWords[newWord];
+            $word1.text(newDW1)
+            $word1.css({marginLeft: "0px"})
+
+        } else if (inputField === newDW2) {
+            newWord = randomInt(displayedWords.length);
+            newDW2 = displayedWords[newWord];
+            $word2.text(newDW2)
+            $word2.css({marginLeft: "0px"})
+
+        } else if (inputField === newDW3) {
+            newWord = randomInt(displayedWords.length);
+            newDW3 = displayedWords[newWord];
+            $word3.text(newDW3)
+            $word3.css({marginLeft: "0px"})
+
+        } else if (inputField === newDW4) {
+            newWord = randomInt(displayedWords.length);
+            newDW4 = displayedWords[newWord];
+            $word4.text(newDW4)
+            $word4.css({marginLeft: "0px"})
+
+        } else if (inputField === newDW5) {
+            newWord = randomInt(displayedWords.length);
+            newDW5 = displayedWords[newWord];
+            $word5.text(newDW5)
+            $word5.css({marginLeft: "0px"})
+
+        } else if (inputField === newDW6) {
+            newWord = randomInt(displayedWords.length);
+            newDW6 = displayedWords[newWord];
+            $word6.text(newDW6)
+            $word6.css({marginLeft: "0px"})
+        } 
+                    } }
+document.querySelector("#input-field").addEventListener("keydown", changeWordOnMatch)
