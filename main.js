777c43c1375bef962f1db9091bc520b88302585b
Jad Georges <jad.grgs@gmail.com>
2018-02-08 10:42:34 -0800

    functionality to switch to player2, new countdown and score track of p2

--- main.js
+++ main.js
@@ -103,3 +103,126 @@ function changeWordOnMatch(e) {
         } 
                     } }
 document.querySelector("#input-field").addEventListener("keydown", changeWordOnMatch)
+
+//functionality for timer
+var timeCount = $("#time-count");
+var time = 20;
+var time2 = 18;
+var p2Turn;
+var waitForYes;
+
+var timer = setInterval(countDown, 1000)
+var timer2;
+
+function stopTimer () {
+    clearInterval(timer);
+    timeCount.text(time) }
+
+function stopTimer2 () {
+    clearInterval(timer2);
+    timeCount.text(time2) }
+
+
+//functionality to count down time for player1, switch to player 2, start a new countdown and track score for player 2
+function countDown() {
+    time = time - 1;
+    timeCount.text(time);
+
+        if (time === 0) {
+            stopTimer();
+
+            clearInterval(word1Move);
+            $word1.css({marginLeft: "0px"});
+
+            clearInterval(word2Move);
+            $word2.css({marginLeft: "0px"});
+
+            clearInterval(word3Move);
+            $word3.css({marginLeft: "0px"});
+            
+            clearInterval(word4Move);
+            $word4.css({marginLeft: "0px"});
+
+            clearInterval(word5Move);
+            $word5.css({marginLeft: "0px"});
+
+            clearInterval(word6Move);
+            $word6.css({marginLeft: "0px"});
+
+                //functionality to switch player
+                p2Turn = prompt("Are you ready p2?");
+                
+
+                while (p2Turn !== "yes" && waitForYes !== "yes") {
+                    waitForYes = prompt("We will wait for a yes")
+                }
+
+                if(p2Turn === "yes" || waitForYes === "yes") {
+                    timer2 = setInterval(countDownAgain, 1000)
+                    function countDownAgain() {
+                        time2 = time2 - 1
+                        timeCount.text(time2);
+                        if(time2 === 0) {
+                            stopTimer2()
+                            checkWinner()
+                            clearInterval(word1Move);
+                            $word1.css({marginLeft: "0px"});
+
+                            clearInterval(word2Move);
+                            $word2.css({marginLeft: "0px"});
+
+                            clearInterval(word3Move);
+                            $word3.css({marginLeft: "0px"});
+                            
+                            clearInterval(word4Move);
+                            $word4.css({marginLeft: "0px"});
+
+                            clearInterval(word5Move);
+                            $word5.css({marginLeft: "0px"});
+
+                            clearInterval(word6Move);
+                            $word6.css({marginLeft: "0px"});
+                        }
+                        
+                    }
+                    
+                        moveNSpeed.move1Fn();
+                        word1Move = setInterval(moveNSpeed.move1Fn, 20);
+
+                        moveNSpeed.move2Fn();
+                        word2Move = setInterval(moveNSpeed.move2Fn, 24);
+
+                        moveNSpeed.move3Fn();
+                        word3Move = setInterval(moveNSpeed.move3Fn, 28);
+
+                        moveNSpeed.move4Fn();
+                        word4Move = setInterval(moveNSpeed.move4Fn, 32);
+
+                        moveNSpeed.move5Fn();
+                        word5Move = setInterval(moveNSpeed.move5Fn, 36);
+
+                        moveNSpeed.move6Fn();
+                        word6Move = setInterval(moveNSpeed.move6Fn, 40);
+                }
+             
+
+                            function checkMatch2(e) {
+                                if(e.keyCode === 13){
+                                    var inputWord2 = $("#input-field").val()
+                                    
+                                    if (inputWord2 === newDW1 || inputWord2 === newDW2 || inputWord2 === newDW3 || inputWord2 === newDW4 || inputWord2 === newDW5 || inputWord2 === newDW6) {
+                                        
+                                        p2Score = p2Score + 1;
+                                        p2ScoreCount.text(p2Score)
+                                        changeWordOnMatch(e);
+                                    }
+                                        inputField = $("#input-field").val("")
+                                        
+                                }
+                                                
+                            }
+                                document.querySelector("#input-field").removeEventListener("keydown", checkMatch)
+                                
+                                document.querySelector("#input-field").addEventListener("keydown", checkMatch2)
+        }
+}
